7644ee235926ace3a9641697e6f934ffaf046a77
nanometre380 <nanometre380>
2025-02-09 02:04:23 +0900

    KAN-8 프로필 이미지 및 텍스트 추가

---
+++ components/ProfileCard.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Image, StyleSheet, View, Text } from "react-native";
+
+interface ProfileCardProps {
+  profileImage?: number | null;
+  profileText?: string;
+}
+
+// 샘플 이미지들 - 추후 서버에서 받아오는 값으로 교체
+const profileImages = [
+  require("../assets/images/profiles/profile1.png"),
+  require("../assets/images/profiles/profile2.png"),
+  require("../assets/images/profiles/profile3.png"),
+  require("../assets/images/profiles/profile4.png"),
+];
+
+const ProfileCard: React.FC<ProfileCardProps> = ({
+  profileImage = 0,
+  profileText = "Unknown User",
+}) => {
+  const profileIndex = profileImage !== null ? profileImage : 0;
+  return (
+    <View style={styles.container}>
+      <Image
+        source={profileImages[profileIndex]}
+        style={styles.profileImage}
+        resizeMode="cover"
+      />
+      <Text style={styles.profileText}>{profileText}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    flexDirection: "row",
+    alignItems: "center",
+    top: 20,
+    left: 20,
+  },
+  profileImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 2,
+    borderColor: "#fff",
+  },
+  profileText: {
+    marginLeft: 10,
+    fontSize: 15,
+    color: "#fff",
+  },
+});
+
+export default ProfileCard;
