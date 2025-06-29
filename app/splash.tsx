f7387aee230677521340c89752ea6352c9cbce13
nanometre380 <nanometre380>
2025-06-29 16:21:12 +0900

    splash screen

--- app/splash.tsx
+++ app/splash.tsx
@@ -1,7 +1,7 @@
 import { useUser } from "@/context/UserContext";
 import { useRouter } from "expo-router";
 import React, { useEffect } from "react";
-import { Text } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
 
 const SplashScreen = () => {
   const router = useRouter();
@@ -36,9 +36,28 @@ const SplashScreen = () => {
   }, []);
   return (
     <>
-      <Text style={{ color: "white" }}>SplashScreen</Text>
+      <View style={styles.container}>
+        <Image
+          source={require("../assets/images/splash-logo.png")}
+          style={styles.logo}
+        />
+      </View>
     </>
   );
 };
 
 export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1F225F",
+  },
+  logo: {
+    height: 100,
+    width: 100,
+    resizeMode: "contain",
+  },
+});
